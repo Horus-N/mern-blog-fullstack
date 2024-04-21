f5064f434588597ed1ecc2d600bc711551d182f1
Nguyen Huu Tung <tungkim614@gmail.com>
2024-04-21 16:27:39 +0700

    api cho login

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -24,4 +24,35 @@ const signup = async(req,res,next)=>{
   }
 }
 
-module.exports = {signup}
+const signin = async(req,res)=>{
+  const {username,password} = req.body;
+
+  if(!username||!password){
+    next(errorHandler(400,"Tất cả các trường không được để trống!"))
+  }
+
+  const findOne = await User.findOne({username});
+  if(!findOne){
+    return res.json({
+      success:false,
+      message:'Tài khoản hoặc mật khẩu không chính xác!'
+    }
+    )
+  }else{
+    const checkPassword = await bcryptjs.compare(req.body.password, findOne.password);
+
+    if(checkPassword){
+      return res.json({
+        success:true,
+        message:'Đăng nhập thành công!'
+      })
+    }else{
+      return res.json({
+        success:false,
+        message:'Tài khoản hoặc mật khẩu không chính xác!'
+      })
+    }
+  }
+}
+
+module.exports = {signup,signin}
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -40,7 +40,7 @@ export default function Header() {
             <FaMoon />
           </Button>
 
-          <Link className="l">
+          <Link to="/sign-in" className="l">
             <Button className="h" gradientDuoTone="purpleToBlue">
               Sign In
             </Button>
@@ -49,13 +49,13 @@ export default function Header() {
         </div>
         <Navbar.Collapse>
           <Navbar.Link active={path==="/"} as={'div'}>
-            <Link to="/">Home</Link>
+            <Link className="w-full h-full block" to="/">Home</Link>
           </Navbar.Link>
           <Navbar.Link active={path==="/about"} as={'div'}>
-            <Link to="/about">About</Link>
+            <Link className="w-full h-full block" to="/about">About</Link>
           </Navbar.Link>
           <Navbar.Link active={path==="/projects"} as={'div'}>
-            <Link to="/projects">Projects</Link>
+            <Link className="w-full h-full block" to="/projects">Projects</Link>
           </Navbar.Link>
         </Navbar.Collapse>
       </Navbar>
