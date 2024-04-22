36849aef8bf85876d4a4647bca0b3d0dc53d4c51
Nguyen Huu Tung <tungkim614@gmail.com>
2024-04-22 15:28:19 +0700

    hoàn thành sidebar dashboard

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,4 +2,6 @@ import Header from "./Header";
 import FooterCom from "./FooterCom";
 import OAuth from "./OAuth";
 import ThemeProvider from "./ThemeProvider";
-export {Header,FooterCom,OAuth,ThemeProvider}
+import DashSidebar from "./DashSidebar";
+import DashProfile from "./DashProfile";
+export {Header,FooterCom,OAuth,ThemeProvider,DashSidebar,DashProfile}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import { DashProfile, DashSidebar } from '../components';
 
 export default function Dashboard() {
+  const location = useLocation();
+ 
+  const [tab,setTab] = useState('');
+
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab')
+    if(tabFromUrl){
+      setTab(tabFromUrl)
+    }
+
+  },[location.search])
   return (
-    <div>Dashboard</div>
+    <div className='w-full md:w-56 flex flex-col md:flex-row min-h-screen '>
+      <div className="md:w-56">
+        {/* sidebar */}
+        <DashSidebar/>
+      </div>
+
+      {/* profile */}
+      {tab==='profile' &&   <DashProfile/>}
+   
+    </div>
   )
 }
